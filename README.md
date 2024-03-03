8f5ee1e51a387c2f9d5b932bbee43e0ac8ed36ec
Michał Sochoń <kaszpir@gmail.com>
2024-03-03 21:36:56 +0100

    Initial commit, probably does not work :)

--- README.md
+++ README.md
@@ -1,2 +1,168 @@
 # prusa-connect-camera-script
+
 Linux shell script to send still camera images to Prusa Connect
+
+## Known limitations
+
+- this script performs processing of the single camera, if you need more cameras
+  then just create multiple copies with different settings (see below)
+- I was not able to test EVERY setting so this may still have some bugs
+- remote streams such as RTSP not tested yet
+- Rpi Zero W or older devices may have CPU limitations to process remote streams
+  or multiple cameras at once
+- Prusa Connect will not show camera image if the printer is not alive, this is
+  Prusa Connect limitation.
+
+## Supported devices
+
+- Raspberry Pi CSI cameras such as [Raspberry Pi Cam](https://www.raspberrypi.com/documentation/accessories/camera.html)
+- most of USB cameras if they work under Linux
+
+## Requirements
+
+- depending on the device you may need to enable camera in the `raspi-config`
+- for USB cameras please install `fswebcam` package
+- generic system packages:
+  - `bash` 5.x (what year is it?)
+  - `git` (just to install scripts from this repo)
+  - `curl`
+  - `iputils-ping`
+
+- optional packages:
+  - `v4l-utils` - to detect camera capabilities
+  - `libraspberrypi-bin` or `rpicam-apps-lite` for Rpi CSI cameras
+  - `fswebcam` - for generic USB cameras
+  - `ffmpeg` - for custom commands for capturing remote streams
+  - you-name-it - for custom commands beyond my imagination
+
+## Installation
+
+```shell
+ssh pi@your-device
+mkdir /home/pi/src
+cd /home/pi/src
+git clone git@github.com:nvtkaszpir/prusa-connect-camera-script.git
+cd prusa-connect-camera-script
+```
+
+## Configuration
+
+Configs are passed as environment vars.
+
+For more in-depth details (no need to repeat them here) please see the top of
+the [prusa-connect-camera.sh](./prusa-connect-camera.sh).
+
+## Adding new camera
+
+Short overview of actions:
+
+- ensure printer is up and running and sending status to Prusa Connect
+  (otherwise images will be discarded)
+- add new camera to the existing printer in Prusa Connect, obtain token
+- create config for prusa-connect-camera-script env vars
+- test the config
+- install script as service
+
+### Create new camera in the Prusa Connect
+
+- go to [Prusa Connect](https://connect.prusa3d.com/) and log in
+- select Printer
+- select Camera
+- on the bottom click `Add new other camera`
+- new camera is created, copy Token, this is needed later
+
+## Create config for prusa-connect-camera-script env vars
+
+There are some example configs depending on what camera you have.
+
+- `csi.dist` - for CSI attached cameras via ribbon to Raspberry Pi
+- `usb.dist` - for generic USB cameras attached via USB to any linux device
+
+Notice that each camera should have different fingerprint and token.
+Do not change fingerprint after launching the script - thus camera is registered
+and you may need to revert the change or delete and readd camera again and start
+from scratch.
+
+How to get info which cameras are available?
+Run `v4l2-ctl --list-devices`
+
+How to get what modes are available for the camera?
+Run `v4l2-ctl --list-formats-ext -d /dev/video0` where `/dev/video0` is a device
+listed from command above, notice not every device is an actual camera.
+
+### CSI camera on Raspberry Pi
+
+- copy `csi.dist` as `.csi` if you want to use Raspberry Pi camera
+- in copied file `.csi` replace `token-change-me` with the value of the token
+  you copied
+- in copied file `.csi` replace `fingerprint-change-me` with some random value,
+  which is alphanumeric and has at least 16 chars (and max of 40 chars),
+  for example set it to `fingerprint-myprinter-camera-1`
+- save edited file `.csi`
+
+### USB camera
+
+- copy `usb.dist` as `.usb` if you want to use Raspberry Pi camera
+
+- in copied file `.usb` replace `token-change-me` with the value of the token
+  you copied
+- in copied file `.usb` replace `fingerprint-change-me` with some random value,
+  which is alphanumeric and has at least 16 chars (and max of 40 chars),
+  for example set it to `fingerprint-myprinter2-camera-2`
+- save edited file `.usb`
+
+## Test the config
+
+- ensure to turn on the 3D Printer so that it sends telemetry, otherwise images
+  will not be seen
+- run below commands, we assume `.env` is the camera config we defined earlier
+
+```shell
+env $(cat .env | xargs) ./prusa-connect-camera.sh
+```
+
+Check for errors, if any, if everything is ok you should see a lot of `204`
+every 10s.
+
+## Install script as service
+
+Depending on the distro there are various options to configure scripts as service
+
+### Configure service with systemd
+
+This project allows spawning multiple systemd units.
+The suffix after `@` defines what env file to load from givven path.
+For example if you set unit file name to `prusa-connect-camera@csi.service`
+then systemd will load env vars from the file under path
+`/home/pi/src/prusa-connect-camera-script/.csi`
+
+So in short:
+
+- copy `csi.dist` as `.csi` and edit it
+- copy `prusa-connect-camera@.service` as `prusa-connect-camera@csi.service`
+- you may additionally edit unit file if you use different config paths
+- run systemd daemon-reload
+- enable systemd service
+- start systemd service
+
+```shell
+cd /home/pi/src/prusa-connect-camera-script/
+cp csi.dist .csi
+# edit .csi and set custom command params, token and fingerprint etc...
+sudo cp prusa-connect-camera@.service /etc/systemd/system/prusa-connect-camera@csi.service
+sudo systemd daemon-reload
+sudo systemctl enable prusa-connect-camera@csi.service
+sudo systemctl status prusa-connect-camera@csi.service
+```
+
+For another camera, let say for another camera attached over USB
+
+```shell
+cd /home/pi/src/prusa-connect-camera-script/
+cp usb.dist .usb1
+# edit .usb1 and set device, token and fingerprint etc...
+sudo cp prusa-connect-camera@.service /etc/systemd/system/prusa-connect-camera@usb1.service
+sudo systemd daemon-reload
+sudo systemctl enable prusa-connect-camera@usb1.service
+sudo systemctl status prusa-connect-camera@usb1.service
+```
---
+++ prusa-connect-camera.sh
@@ -0,0 +1,184 @@
+#!/usr/bin/env bash
+
+# basic config
+
+# sleep time in seconds between image captures, notice that PrusaConnect accepts images at most every 10s or slower
+: "${SLEEP:=10}"
+
+# Printer address to ping
+# if address is unreachable there is no point in sending an image
+# set to 127.0.0.1 to always send images
+# set to empty value to disable this feature
+: "${PRINTER_ADDRESS:=127.0.0.1}"
+
+# PrusaConnect API key
+: "${PRUSA_CONNECT_CAMERA_TOKEN:=unset}"
+# PrusaConnect camera fingerprint, use for example 'uuidgen' to generate it, it must be at least 16 alphanumeric chars, 40 max
+: "${PRUSA_CONNECT_CAMERA_FINGERPRINT:=unset}"
+
+# camera device to use, if you use camera attached to the CSI via camera ribbon then leave as is
+: "${CAMERA_DEVICE:=/dev/video0}"
+
+# avaliable options:
+# rpicam-still - using CSI camera + modern Rasberry Pi operating systems since Debian 11 Bullseye
+# raspistill - using CSI camera + older Raspberyy Pi operating systems
+# fswebcam - using USB camera + custom package 'fswebcam'
+# anything else will be processed directly, so for example you could use 'ffmpeg' in here
+: "${CAMERA_COMMAND:=rpicam-still}"
+
+# extra params passed to the camera program, passed directly as <command> <extra-params> <output_file>
+# libcamera (rpicam-still)
+: "${CAMERA_COMMAND_EXTRA_PARAMS:=--immediate --nopreview --mode 2592:1944:12:P --lores-width 0 --lores-height 0 --thumb none -o }"
+# raspistill
+#: "${CAMERA_COMMAND_EXTRA_PARAMS:=--nopreview --mode 2592:1944:12:P -o }"
+# fswebcam
+#: "${CAMERA_COMMAND_EXTRA_PARAMS:=--resolution 1280x960 --no-banner }"
+# ffmpeg, in this case CAMERA_DEVICE is ignored, use it directly in the extra params
+#: "${CAMERA_COMMAND_EXTRA_PARAMS:=-f v4l2 -y -i /dev/video0 -f image2 -vframes 1 -pix_fmt yuvj420p }"
+
+# where to save camera images (they will be overwritten every time
+: "${TARGET_DIR:=/dev/shm}"
+# extra params to curl when pushing an image
+: "${CURL_EXTRA_PARAMS:=}"
+
+## end of config, do not modify (unless you knw what you are doing)
+: "${PRUSA_CONNECT_URL:=https://webcam.connect.prusa3d.com/c/snapshot}"
+
+# validators
+if ! [[ -w "${TARGET_DIR}" ]]; then
+  echo "ERROR: ${TARGET_DIR} is not writable"
+  echo "If you run in read only systems then ensure to mount ${TARGET_DIR} as write volume"
+  exit 1
+fi
+
+if [[ "${PRUSA_CONNECT_CAMERA_TOKEN}" = "unset" ]]; then
+  echo "ERROR: PRUSA_CONNECT_CAMERA_TOKEN env var is not set, please set it in the script or in the env vars"
+  echo "Go to PrusaConnect site and add camera, copy Token and set in the script or in the env vars"
+  exit 1
+fi
+
+if [[ "${PRUSA_CONNECT_CAMERA_FINGERPRINT}" = "unset" ]]; then
+  echo "ERROR: PRUSA_CONNECT_CAMERA_FINGERPRINT env var is not set, please set it in the script or in the env vars"
+  echo "You can use 'uuidgen' shell command to generate unique id, remember not to change it aferwards (or delete and add camera)"
+  exit 1
+fi
+
+camera_id="${PRUSA_CONNECT_CAMERA_FINGERPRINT}"
+
+
+if [[ ! -r "${CAMERA_DEVICE}" ]]; then
+  echo "ERROR: Could not read camera device ${CAMERA_DEVICE}"
+  echo "Are you sure current user can read that file?"
+  echo "Did you enable camera in raspi-config and did reboot?"
+  echo "Does the device exist? Check for missing kernel modules, disconnected cables..."
+  echo "Use command "
+  echo "  v4l2-ctl --list-devices"
+  echo "to list available devices"
+  echo "Also please see https://www.raspberrypi.com/documentation/computers/camera_software.html"
+  exit 1
+fi
+
+# detect missing dependencies depending on camera capture method
+if [[ "${CAMERA_COMMAND}" = "raspistill" ]]; then
+  if type raspistill >/dev/null 2>/dev/null; then
+    # older distros
+    # ignoring CAMERA_DEVICE
+    echo "Using raspistill"
+    command_capture="raspistill"
+  else
+    echo "ERROR: Missing raspi-still command."
+    echo "Run 'sudo apt install -y libraspberrypi-bin'"
+    echo "Run 'raspi-config' and enable camera in the menu, then enable I2C and reboot device."
+    exit 1
+  fi
+fi
+
+if [[ "${CAMERA_COMMAND}" = "rpicam-still" ]]; then
+  if type rpicam-still >/dev/null 2>/dev/null; then
+    # newer distros
+    # need to get number from device as camera_device
+    camera_number=$(echo "${CAMERA_DEVICE}" | grep -oP '\d+')
+    echo "Using libcamera"
+    command_capture="rpicam-still --camera ${camera_number}"
+  else
+    echo "ERROR: Missing raspicam-still command."
+    echo "Run 'sudo apt install -y rpicam-apps-lite'"
+    echo "Run 'sudo raspi-config' and enable camera in the menu, then enable I2C and reboot device."
+    exit 1
+  fi
+fi
+
+if [[ "${CAMERA_COMMAND}" = "fswebcam" ]]; then
+  if type fswebcam >/dev/null 2>/dev/null; then
+    # newer distros
+    echo "Using fswebcam"
+    command_capture="fswebcam -d ${CAMERA_DEVICE}"
+  else
+    echo "ERROR: missing fswebcam package"
+    echo "Run 'sudo install -y fswebcam'"
+    exit 1
+  fi
+fi
+
+if [[ -z "${command_capture}" ]]; then
+  if type ${CAMERA_COMMAND} >/dev/null 2>/dev/null ; then
+    echo "Using $CAMERA_COMMAND}"
+    command_capture="${CAMERA_COMMAND}"
+  else
+    echo "ERROR: missing ${CAMERA_COMMAND}"
+    echo "No idea how to fix it because appareently this is a custom command."
+    exit 1
+  fi
+fi
+
+
+if [[ -z "${command_capture}" ]]; then
+  echo "ERROR: Could not detect image capture method, aborting"
+  exit 1
+fi
+
+echo "Camera capture command: ${command_capture} ${CAMERA_COMMAND_EXTRA_PARAMS} ${TARGET_DIR}/camera_${camera_id}.jpg"
+
+
+trap "echo SIGINT received, exiting...; exit 0" INT
+
+echo "Starting camera loop..."
+while true; do
+  if [[ -n "${PRINTER_ADDRESS}" ]]; then
+    # check if printer is up
+    if ! ping -q -4 -c1 -n -w 2 "${PRINTER_ADDRESS}" > /dev/null; then
+      echo "WARNING: Printer not responding to ping at address ${PRINTER_ADDRESS}, skipping image capture"
+      sleep "${SLEEP}"
+      continue
+    fi
+  fi
+
+  # perform image capture, write to /dev/shm to avoid killing slowly microSD card with writes
+  eval "${command_capture}" "${CAMERA_COMMAND_EXTRA_PARAMS}" "${TARGET_DIR}/camera_${camera_id}.jpg" \
+    >"${TARGET_DIR}/camera_${camera_id}.stdout" \
+    2>"${TARGET_DIR}/camera_${camera_id}.stderr"
+  result=$?
+  if [[ ${result} -eq 0 ]]; then
+    # get captured image size, this is required by PrusaConnect API
+    image_size=$(stat --printf="%s"  "${TARGET_DIR}/camera_${camera_id}.jpg")
+    # push image to PrusaConnect, print return code, it should be 204 if all is ok, if not then you will see error message
+    curl -X PUT "${PRUSA_CONNECT_URL}" \
+      -H "Accept: text/plain" \
+      -H "Content-type: image/jpg" \
+      -H "Fingerprint: ${PRUSA_CONNECT_CAMERA_FINGERPRINT}" \
+      -H "Token: ${PRUSA_CONNECT_CAMERA_TOKEN}" \
+      -H "Content-length: ${image_size}" \
+      --data-binary "@${TARGET_DIR}/camera_${camera_id}.jpg" \
+      --no-progress-meter \
+      --compressed \
+      -w "%{http_code}\n" \
+      ${CURL_EXTRA_PARAMS}
+  else
+    # oh noes!
+    echo "camera image capture command encountered an error, for more details see:"
+    echo "${TARGET_DIR}/camera_${camera_id}.stdout"
+    echo "${TARGET_DIR}/camera_${camera_id}.stderr"
+  fi
+
+  sleep "${SLEEP}"
+done
